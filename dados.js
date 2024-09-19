ca292954b1261f7cda84223fe3414b9cd19fb685
Guilherme Castelo <guilherme.senacastelo@gmail.com>
2024-09-18 23:39:28 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,91 +2,91 @@ let paises = [
     {
         nome: "Brasil",
         bandeira: "brasil.png",
-        descricao: "O Brasil é um país continental com uma rica diversidade cultural e natural.",
+        descricao: "O Brasil é um país sul-americano de dimensões continentais, famoso por sua rica biodiversidade, especialmente a Amazônia. Com uma cultura vibrante e marcada pela miscigenação, o país é um dos mais diversos do mundo. Sua economia, uma das maiores do planeta, se baseia em setores como agricultura, indústria e serviços.",
         tags: "Feijoada Coxinha Farofa"
     },
     {
         nome: "Argentina",
         bandeira: "argentina.png",
-        descricao: "A Argentina, um país da América do Sul, é famosa por sua paixão pelo futebol, o tango e os vinhos finos.",
+        descricao: "A Argentina é um país da América do Sul, conhecido por sua rica cultura, história e paisagens diversas. Com uma extensão territorial significativa, o país abriga desde as montanhas dos Andes até as vastas planícies dos Pampas. Sua capital, Buenos Aires, é um centro cultural vibrante, famoso pelo tango e pela arquitetura europeia.",
         tags: "Parrillada"
     },
     {
         nome: "Estados Unidos",
         bandeira: "estados-unidos.png",
-        descricao: "Os Estados Unidos são uma potência mundial com uma economia diversificada e uma cultura popular que influencia o mundo todo.",
+        descricao: "Os Estados Unidos são uma nação da América do Norte, reconhecida como a maior potência econômica e militar do mundo. Com uma diversidade cultural imensa, fruto de séculos de imigração, o país abriga uma variedade de paisagens, desde as montanhas Rochosas até as praias da Califórnia.",
         tags: "Hambúrguer"
     },
     {
         nome: "Canadá",
         bandeira: "canada.png",
-        descricao: "O Canadá, um país norte-americano, é famoso por suas paisagens montanhosas, florestas boreais e cidades multiculturales.",
+        descricao: "O Canadá, um país da América do Norte, é conhecido por suas paisagens deslumbrantes, sua alta qualidade de vida e sua diversidade cultural. Com uma capital, Ottawa, e cidades cosmopolitas como Toronto e Montreal, o Canadá oferece uma experiência única para cada visitante.",
         tags: "Poutine"
     },
     {
         nome: "México",
         bandeira: "mexico.png",
-        descricao: "O México é um país da América do Norte, conhecido por sua rica história, sua cultura vibrante e suas belas praias.",
+        descricao: "O México, um país da América do Norte, é uma verdadeira fusão de culturas, paisagens e sabores. Com uma rica história que se estende por milênios, o México oferece uma experiência única para cada visitante.",
         tags: "Tacos"
     },
     {
         nome: "França",
         bandeira: "franca.png",
-        descricao: "A França é um país europeu famoso por sua cultura, moda, gastronomia e vinhos.",
+        descricao: "A França, localizada no oeste da Europa, é um país que encanta por sua rica história, cultura vibrante e paisagens diversificadas. Com uma capital, Paris, conhecida mundialmente por sua beleza e romance, a França é um destino turístico imperdível.",
         tags: "Croissant"
     },
     {
         nome: "Alemanha",
         bandeira: "alemanha.png",
-        descricao: "A Alemanha é conhecida por sua história rica, sua indústria automobilística e sua cerveja.",
+        descricao: "A Alemanha, localizada no coração da Europa, é um país que encanta por sua rica história, cultura vibrante e paisagens diversificadas. Com uma capital, Berlim, que carrega as marcas do passado e da modernidade, a Alemanha é um destino que oferece uma experiência única aos seus visitantes.",
         tags: "Currywurst"
     },
     {
         nome: "Itália",
         bandeira: "italia.png",
-        descricao: "A Itália é um país europeu com uma rica história e cultura.",
+        descricao: "A Itália, com sua rica história, arte vibrante e paisagens deslumbrantes, é um dos destinos turísticos mais populares do mundo. Do Coliseu romano às paisagens bucólicas da Toscana, a Itália oferece uma experiência única para cada visitante.",
         tags: "Pizza"
     },
     {
         nome: "Espanha",
         bandeira: "espanha.png",
-        descricao: "A Espanha é um país europeu com uma rica história e cultura.",
+        descricao: "A Espanha, um país localizado na Península Ibérica, é um destino que encanta por sua rica história, cultura vibrante e paisagens diversificadas. Com uma capital, Madrid, que pulsa com vida e arte, a Espanha oferece uma experiência única para cada visitante.",
         tags: "Paella"
     },
     {
         nome: "Reino Unido",
         bandeira: "reino-unido.png",
-        descricao: "O Reino Unido é um país insular na Europa, conhecido por sua história rica, sua cultura diversificada e sua língua inglesa.",
+        descricao: "O Reino Unido, uma ilha localizada na costa noroeste da Europa, é um país com uma rica história, cultura vibrante e paisagens diversificadas. Formado por Inglaterra, Escócia, País de Gales e Irlanda do Norte, o Reino Unido oferece aos visitantes uma experiência única, desde as movimentadas ruas de Londres até as paisagens bucólicas da Escócia.",
         tags: "Fish and chips"
     },
     {
         nome: "Japão",
         bandeira: "japao.png",
-        descricao: "O Japão é um arquipélago no Oceano Pacífico, conhecido por sua cultura milenar e tecnologia avançada.",
+        descricao: "O Japão, um arquipélago localizado no Oceano Pacífico, é um país que encanta por sua rica história, cultura milenar e paisagens deslumbrantes. Com uma capital, Tóquio, que é uma metrópole vibrante e moderna, o Japão oferece uma experiência única para cada visitante.",
         tags: "Sushi"
     },
     {
         nome: "China",
         bandeira: "china.png",
-        descricao: "A China é uma das civilizações mais antigas do mundo, com uma rica história e cultura.",
+        descricao: "A China, um país asiático de vastas dimensões e rica história, é uma das civilizações mais antigas do mundo. Com uma capital, Pequim, que combina a arquitetura moderna com locais históricos, a China oferece uma experiência única para cada visitante.",
         tags: "Chow Mein"
     },
     {
         nome: "Índia",
         bandeira: "india.png",
-        descricao: "A Índia é um país diverso com uma rica história e cultura.",
+        descricao: "A Índia, um país localizado no subcontinente indiano, na Ásia, é conhecida por sua rica história, cultura diversificada e paisagens deslumbrantes. Com uma capital, Nova Déli, que combina a modernidade com a tradição, a Índia oferece uma experiência única para cada visitante.",
         tags: "Samosa"
     },
     {
         nome: "Rússia",
         bandeira: "russia.png",
-        descricao: "A Rússia, o maior país do mundo em extensão territorial, possui uma história rica e complexa.",
+        descricao: "A Rússia, com sua vasta extensão territorial que se estende por dois continentes, a Europa e a Ásia, é um país que encanta e intriga em igual medida. Sua rica história, cultura vibrante e paisagens diversas oferecem uma experiência única para cada visitante.",
         tags: "Solyanka"
     },
     {
         nome: "Austrália",
         bandeira: "australia.png",
-        descricao: "A Austrália, um continente-ilha, é conhecida por sua fauna única, como o canguru e o coala.",
+        descricao: "A Austrália, um país insular localizado na Oceania, é conhecida por sua rica biodiversidade, paisagens deslumbrantes e cultura vibrante. Com uma capital, Canberra, e cidades cosmopolitas como Sydney e Melbourne, a Austrália oferece uma experiência única para cada visitante.",
         tags: "Vegemite"
     },
 ];
